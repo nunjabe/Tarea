b7c011350aec674165c324002291987120ad14c5
admin <admin@HP255G8>
2024-10-21 18:54:42 -0600

    Add project files.

---
+++ Tarea/Models/Persona.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Tarea.Models
+{
+    public class Persona
+    {
+        [Required(ErrorMessage = "# Cuenta es requerido.")]
+        [Range(1, int.MaxValue, ErrorMessage = "# Cuenta debe ser mayor que 0.")]
+        public int? Num_cuenta { get; set; }
+
+        [Required(ErrorMessage = "Nombre no debe ser vacío.")]
+        [MaxLength(100, ErrorMessage = "El nombre no puede exceder 100 caracteres.")]
+        public string? Nombre { get; set; }
+
+        [Required(ErrorMessage = "Correo no debe ser vacío.")]
+        [EmailAddress(ErrorMessage = "Correo debe ser un correo válido.")]
+        public string? Correo { get; set; }
+
+        [Range(0, 10, ErrorMessage = "Calificación debe estar entre 0 y 10.")]
+        public int? Calificacion { get; set; }
+    }
+}
